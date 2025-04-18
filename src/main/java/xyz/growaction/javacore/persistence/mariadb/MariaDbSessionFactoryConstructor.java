5f18fc16e2f338c1a88559a220e64a132cbb8fb4
Till Hoffmann <mail@tillhfm.de>
2025-04-18 15:15:25 +0200

    chore: optimize imports

--- src/main/java/xyz/growaction/javacore/persistence/mariadb/MariaDbSessionFactoryConstructor.java
+++ src/main/java/xyz/growaction/javacore/persistence/mariadb/MariaDbSessionFactoryConstructor.java
@@ -1,6 +1,5 @@
 package xyz.growaction.javacore.persistence.mariadb;
 
-import lombok.Builder;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.jetbrains.annotations.NotNull;
