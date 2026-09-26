8f95a71436a7aac0a64330ac43fb6be155f3bc2f
Till Hoffmann <till@hofm.cloud>
2026-09-26 00:38:37 +0000

    build: count package cycles between feature packages, not every package

--- src/test/java/eu/nordtal/jcore/ArchitectureTest.java
+++ src/test/java/eu/nordtal/jcore/ArchitectureTest.java
@@ -24,7 +24,7 @@ class ArchitectureTest {
 
     @Test
     void packagesHaveNoCycles() {
-        slices().matching("eu.nordtal.jcore.(**)").should().beFreeOfCycles().check(classes);
+        slices().matching("eu.nordtal.jcore.(*).(*)..").should().beFreeOfCycles().check(classes);
     }
 
     @Test
